664f1f36cf9758c1c1981c3b2a25472668ef3bdd
Jorge <jorgejapao00@gmail.com>
2024-04-06 12:59:02 -0300

    Embed for help command added

---
+++ embeds.py
@@ -0,0 +1,24 @@
+helpe = {
+  "embeds": [
+    {
+      "id": 652627557,
+      "title": "About Colisa AI Bot",
+      "description": "Colisa is a powerful discord bot powered with Google's Gemini AI! That enables you to ask her anything you want!",
+      "color": 15410105,
+      "fields": [
+        {
+          "id": 886545035,
+          "name": "**Welcome to the Help Screen!**",
+          "value": "Teste"
+        }
+      ],
+      "url": "https://github.com/horue/Colisa-The-AI-Discord-Bot",
+      "timestamp": "2024-04-06T03:00:00.000Z",
+      "footer": {
+        "text": "Bot developed by horue. Gemini AI developed by Google."
+      }
+    }
+  ],
+  "components": [],
+  "actions": {}
+}
