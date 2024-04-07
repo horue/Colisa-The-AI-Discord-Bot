b3f2c350182954c7746adfe07cf6177c3caaa0af
Jorge <jorgejapao00@gmail.com>
2024-04-06 21:09:36 -0300

    Removed worng embed

--- embeds.py
+++ embeds.py
@@ -1,35 +1,6 @@
 import discord
 from discord.ext import commands
 
-
-
-ehelpe = {
-  "embeds": [
-    {
-      "id": 652627557,
-      "title": "About Colisa AI Bot",
-      "description": "Colisa is a powerful discord bot powered with Google's Gemini AI! That enables you to ask her anything you want!",
-      "color": 15410105,
-      "fields": [
-        {
-          "id": 886545035,
-          "name": "**Welcome to the Help Screen!**",
-          "value": "Teste"
-        }
-      ],
-      "url": "https://github.com/horue/Colisa-The-AI-Discord-Bot",
-      "timestamp": "2024-04-06T03:00:00.000Z",
-      "footer": {
-        "text": "Bot developed by horue. Gemini AI developed by Google."
-      }
-    }
-  ],
-  "components": [],
-  "actions": {}
-
-}
-
-
 helpe = discord.Embed(
     title = "Needing some help? Let's go!",
     description = 'Here you can find an overview of what I can do.\n For now, I am still at the start of my developtment, but the AI features of conversation are working.',
